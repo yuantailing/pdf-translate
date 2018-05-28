577ace2fa50b55856c006aae5d9e6d4b82e1e40c
yuantailing <yuantailing@126.com>
2018-05-28 20:09:13 +0800

    better build

--- build.sh
+++ build.sh
@@ -8,9 +8,16 @@ cp -r pdf.js/build/generic/* build/pdf.js/
 # copy assets
 cp -r www/* build/
 
+# download required JS
+mkdir -p build/static/js
+pushd . && cd build/static/js
+curl -sS https://unpkg.com/jquery/dist/jquery.min.js -ojquery.min.js
+curl -sS https://unpkg.com/vue/dist/vue.min.js -ovue.min.js
+popd
+
 # download welcome PDF
 mkdir -p build/assets
-curl https://arxiv.org/pdf/1805.08090.pdf -obuild/assets/welcome.pdf
+curl -sS https://arxiv.org/pdf/1805.08090.pdf -obuild/assets/welcome.pdf
 
 # build webpages
 python src/build.py
--- src/build.py
+++ src/build.py
@@ -5,14 +5,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from six.moves import urllib
+
 
 def main():
     with open('build/pdf.js/web/viewer.html', 'rb') as f:
         content = f.read()
-    content = content.replace(br'<link rel="stylesheet" href="viewer.css">', br'<link rel="stylesheet" href="pdf.js/web/viewer.css">')
-    content = content.replace(br'<link rel="resource" type="application/l10n" href="locale/locale.properties">', br'<link rel="resource" type="application/l10n" href="pdf.js/web/locale/locale.properties">')
-    content = content.replace(br'<script src="../build/pdf.js"></script>', br'<script src="pdf.js/build/pdf.js"></script>')
-    content = content.replace(br'<script src="viewer.js"></script>', br'<script src="pdf.js/web/viewer.js"></script>')
+    for url_old in ('viewer.css', 'locale/locale.properties', '../build/pdf.js', 'viewer.js', 'pdf.viewer.js'):
+        url_new = urllib.parse.urljoin('pdf.js/web/', url_old)
+        bin_old = '"{:s}"'.format(url_old).encode()
+        bin_new = '"{:s}"'.format(url_new).encode()
+        assert content.count(bin_old) <= 1
+        content = content.replace(bin_old, bin_new)
     with open('src/index-addon.html', 'rb') as f:
         addon = f.read()
     insertBeforeLine = br'</body>'
