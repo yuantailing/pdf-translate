aeabc09ab2f3c0075a57611ee4f50516b4e6b93a
yuantailing <yuantailing@126.com>
2018-05-28 21:50:02 +0800

    basic demo

--- build.sh
+++ build.sh
@@ -11,13 +11,13 @@ cp -r www/* build/
 # download required JS
 mkdir -p build/static/js
 pushd . && cd build/static/js
-curl -sS https://unpkg.com/jquery/dist/jquery.min.js -ojquery.min.js
-curl -sS https://unpkg.com/vue/dist/vue.min.js -ovue.min.js
+curl -sSL https://unpkg.com/jquery/dist/jquery.js -ojquery.js
+curl -sSL https://unpkg.com/vue/dist/vue.js -ovue.js
 popd
 
 # download welcome PDF
 mkdir -p build/assets
-curl -sS https://arxiv.org/pdf/1805.08090.pdf -obuild/assets/welcome.pdf
+curl -sSL https://arxiv.org/pdf/1805.08090.pdf -obuild/assets/welcome.pdf
 
 # build webpages
 python src/build.py
